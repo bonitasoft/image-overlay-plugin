c7049abc69678a464c122abbd0d8063a4a09122f
Romain Bioteau <romain.bioteau@bonitasoft.com>
2019-04-15 14:32:01 +0200

    add release pipeline

---
+++ infrastrucutre/release.groovy
@@ -0,0 +1,18 @@
+timestamps {
+    node {
+        stage('Setup') {
+            checkout scm
+        }
+
+        stage('Release version') {
+              withCredentials([usernamePassword(
+                        credentialsId: 'github',
+                        passwordVariable: 'GIT_PASSWORD',
+                        usernameVariable: 'GIT_USERNAME')]) {
+                    sh """
+                            ./mvnw -B release:prepare release:perform -Darguments="-DaltDeploymentRepository=${env.ALT_DEPLOYMENT_REPOSITORY_TAG}"
+                            """
+              }
+         }
+    }
+}
